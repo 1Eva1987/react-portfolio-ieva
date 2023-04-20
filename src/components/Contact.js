58926c714afd4ef7bdf43604f43687232b1c6e6f
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-04-20 17:08:40 +0100

    icons added to skills component, contact page created in contact component useing emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,66 @@
 import React from "react";
+import { FaPhone, FaGithub, FaLinkedin } from "react-icons/fa";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_cfh7eub",
+        "template_t9jcazh",
+        form.current,
+        "YSOXrxNZKaJKKFM8q"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent!");
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <div>
-      <h1>CONTACT ME</h1>
+    <div className="contacts">
+      <div className="download">
+        <a className="cv" href="./Images/Ieva.B-CV.pdf" download>
+          Download CV
+        </a>
+        <h2>And</h2>
+        <h2 className="h1">Contact Me</h2>
+      </div>
+      <div className="contact-me">
+        <div className="contact-links">
+          <a className="c-icon" href="tel:+447397803602">
+            <FaPhone />
+          </a>
+          <a className="c-icon" href="https://github.com/1Eva1987">
+            <FaGithub />
+          </a>
+          <a
+            className="c-icon"
+            href="https://www.linkedin.com/in/ieva-baranauskaite-096773252"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <p>Send me a message! </p>
+          <p>I'd love to hear from you.</p>
+          <input type="text" name="user_name" placeholder="Name?" />
+          <input type="email" name="user_email" placeholder="E-mail" />
+          <textarea name="message" placeholder="Your message..." />
+          <input className="submit" type="submit" value="Send" />
+        </form>
+      </div>
     </div>
   );
 }
-
 export default Contact;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,49 @@
 import React from "react";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaBootstrap,
+  FaJsSquare,
+  FaReact,
+  FaNodeJs,
+  FaGithub,
+} from "react-icons/fa";
+import { SiJquery } from "react-icons/si";
+import { TbBrandVscode } from "react-icons/tb";
 
 function Skills() {
   return (
     <div>
       <h1>SKILLS</h1>
+      <div>
+        <div>
+          <FaHtml5 />
+        </div>
+        <div>
+          <FaCss3Alt />
+        </div>
+        <div>
+          <FaBootstrap />
+        </div>
+        <div>
+          <FaJsSquare />
+        </div>
+        <div>
+          <SiJquery />
+        </div>
+        <div>
+          <FaGithub />
+        </div>
+        <div>
+          <TbBrandVscode />
+        </div>
+        <div>
+          <FaNodeJs />
+        </div>
+        <div>
+          <FaReact />
+        </div>
+      </div>
     </div>
   );
 }

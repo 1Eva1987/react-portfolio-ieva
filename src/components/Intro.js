46e6ea5eb22f86a5a3a1671e65831fe78bab196d
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-04-21 13:42:45 +0100

    intro component style applied

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import "../pages/Home.css";
+import "../css/intro.css";
 import Typewriter from "typewriter-effect";
 
 function Intro() {
@@ -29,14 +29,15 @@ function Intro() {
         <h2>
           My name is <span>Ieva</span>
         </h2>
-
         <p>I'm a Junior Developer</p>
-        <p>based in </p>
+        <p>based in</p>
         <p>North Lincolnshire</p>
-        <p className="question">Want to know more?</p>
+        <p className="question">Want to know MORE?</p>
         <p className="more">
-          Feell free to use the <span>LINKS </span>above.
+          Feell free to use the <span>LINKS </span>above
         </p>
+        <p>or</p>
+        <p className="scroll">KEEP SCROLLING</p>
       </div>
     </div>
   );
